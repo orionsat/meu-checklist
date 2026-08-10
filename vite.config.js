99841449ab4a91983d2a442a8ec028ff3ca2f948
Luis Felipe <adm@orionsatgestao.com.br>
2026-08-10 13:40:18 -0300

    Fix: libera hosts no vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs.plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  preview: {
+    allowedHosts: true, // 👈 Libera qualquer domínio no modo preview
+  },
+  server: {
+    allowedHosts: true,
+  }
 })
